7908b605c088476f4118f9b866b4c59bfe0921b1
ivanatrpeska5 <itrpeska5@gmail.com>
2023-07-21 15:09:14 +0200

    many-to-many-category-food

--- backend/src/main/kotlin/com/sorsix/backend/model/Food.kt
+++ backend/src/main/kotlin/com/sorsix/backend/model/Food.kt
@@ -27,7 +27,12 @@ class Food(
     )
     val ingredientsList: MutableList<Ingredient> = arrayListOf(),
 
-
-
-
+    @ManyToMany
+    @JsonManagedReference
+    @JoinTable(
+        name = "category_of_food",
+        joinColumns = [JoinColumn(name = "foodId")],
+        inverseJoinColumns = [JoinColumn(name = "categoryId")],
+    )
+    val categoryList: MutableList<Category> = arrayListOf(),
 )
