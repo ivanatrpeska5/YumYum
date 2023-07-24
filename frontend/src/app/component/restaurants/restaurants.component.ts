d85d460af9406177f2f7071907c612032039ac94
Tosic <toshestefan@gmail.com>
2023-07-24 14:09:39 +0200

    restaurant component

--- frontend/src/app/component/restaurants/restaurants.component.ts
+++ frontend/src/app/component/restaurants/restaurants.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Restaurant } from 'src/app/model/restaurant';
+import { RestaurantsService } from 'src/app/service/restaurants.service';
 
 @Component({
   selector: 'app-restaurants',
   templateUrl: './restaurants.component.html',
   styleUrls: ['./restaurants.component.css']
 })
-export class RestaurantsComponent {
+export class RestaurantsComponent implements OnInit {
+
+  restaurants:Restaurant[] | undefined;
+
+  ngOnInit(): void {
+    this.getRestaurants()
+  }
+
+  constructor(private restaurantsService:RestaurantsService){
+
+  }
+
+  getRestaurants(){
+    this.restaurantsService.getRestaurants().subscribe(restaurants=>{
+      this.restaurants=restaurants
+    })
+  }
 
 }
