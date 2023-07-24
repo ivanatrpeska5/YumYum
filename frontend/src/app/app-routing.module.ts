af08cfc2ca50d9cd04c384174c7afe42f08f0a42
ivanatrpeska5 <itrpeska5@gmail.com>
2023-07-24 13:39:18 +0200

    stefan-da-vidi

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationGuard } from './authentication.guard';
 import { HomeComponent } from './component/home/home.component';
 import { LoginComponent } from './component/login/login.component';
+import { RestaurantComponent } from './component/restaurant/restaurant.component';
 
 const routes: Routes = [
   {path: '', canActivate:[AuthenticationGuard], children: [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
-    { path: '**', redirectTo: '' }
+    { path: '**', redirectTo: '' },
+    {path:'restaurants', component: RestaurantComponent},
   ]}
 ];
 
