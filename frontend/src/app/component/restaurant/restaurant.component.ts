8f2394f47cd584137c785b8651a8f48159e1a190
ivanatrpeska5 <itrpeska5@gmail.com>
2023-07-29 18:34:45 +0200

    not-logged-user-error-quantity-less-than-0-error-messages

--- frontend/src/app/component/restaurant/restaurant.component.ts
+++ frontend/src/app/component/restaurant/restaurant.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Food } from 'src/app/model/food';
 import { FoodsByCategory } from 'src/app/model/foodsByCategory';
 import { Restaurant } from 'src/app/model/restaurant';
 import { CartService } from 'src/app/service/cart.service';
@@ -17,16 +18,18 @@ export class RestaurantComponent implements OnInit {
   foodsByCategory:FoodsByCategory[]=[];
   quantity:{[id:number]:number}={}
   sessionId: string | null = null;
-  
+
   ngOnInit(): void {
     this.getRestaurant()
     this.getFoodsbyCategory()
     this.quantity={}
     this.sessionId=localStorage.getItem('token');
+    this.initForms();
   }
 
   constructor(private restaurantsService:RestaurantsService, 
               private route: ActivatedRoute,
+              private formBuilder: FormBuilder,
               private cartService:CartService){
 
   }
@@ -45,10 +48,37 @@ export class RestaurantComponent implements OnInit {
     })
   }
 
+  createFoodForm(foodId: number): FormGroup {
+    return this.formBuilder.group({
+      foodId: [foodId],
+      quantity: ['1', Validators.required],
+    });
+  }
+
+  initForms() {
+    this.foodsByCategory.forEach((category) => {
+      category.food.forEach((food) => {
+        this.quantity[food.id] = 1; // Set default quantity to 1 for each food item
+      });
+    });
+  }
+
+  onQuantityChange(foodId: number, quantity: number) {
+    this.quantity[foodId] = quantity;
+  }
+
   addToCart(foodId:number){
     console.log(foodId)
-    this.cartService.addToCart(foodId,this.quantity[foodId])
+    var food_quantity=this.quantity[foodId]
+    console.log(food_quantity)
+    if(food_quantity==undefined){
+      food_quantity=1
+    }
+    if(food_quantity>0){
+      this.cartService.addToCart(foodId,food_quantity)
+    }
   }
+
 
   isAuthenticated():boolean{
     return this.sessionId!=null;
